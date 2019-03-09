37a687e7235521fb4c3045030676c2f32be4a218
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-10 00:49:27 +0300

    chore: register comments admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Comment, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -12,4 +12,11 @@ class PostAdmin(admin.ModelAdmin):
 	ordering = ('status', 'publish')
 
 
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('name', 'email', 'post', 'created', 'active')
+	list_filter = ('created', 'active', 'updated')
+	search_fields = ('name', 'email', 'post', 'body')
+
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post, PostAdmin)
