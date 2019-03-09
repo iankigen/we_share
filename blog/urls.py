abfdf870b286eca2757cc3f979ad22e9aa7068a4
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-09 19:35:26 +0300

    chore: add url patterns for blog

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import post_detail, post_list
+
+urlpatterns = [
+	url(r'^$', post_list, name='post_list'),
+	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', post_detail, name='post_detail')
+]
