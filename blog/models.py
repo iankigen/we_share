3e577aeeccbd0c4f97611baff690ff266ebfabc9
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-09 18:39:32 +0300

    chore: add post model and migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+	DRAFT = 'D'
+	PUBLISHED = 'P'
+	SUSPENDED = 'S'
+	STATUS_CHOICES = (
+		(DRAFT, 'Draft'),
+		(PUBLISHED, 'Published'),
+		(SUSPENDED, 'Suspended'),
+	)
+
+	title = models.CharField(max_length=255)
+	slug = models.SlugField(max_length=255, unique_for_date='publish')
+	author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+	body = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=DRAFT)
+
+	class Meta:
+		ordering = ('-publish',)
+
+	def __str__(self):
+		return self.title
