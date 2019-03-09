492186ef3cb199fa958e4eb59c3be1afa7c2c5df
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-10 00:12:56 +0300

    chore: add email share ability

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import post_detail, PostListView
+from .views import post_detail, PostListView, post_share
 
 app_name = 'blog'
 
 urlpatterns = [
 	path('', PostListView.as_view(), name='post_list'),
-	path('<int:year>/<int:month>/<int:day>/<slug:post>', post_detail, name='post_detail')
+	path('<int:year>/<int:month>/<int:day>/<slug:post>', post_detail, name='post_detail'),
+	path('<int:post_id>/share/', post_share, name='post_share')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -25,4 +26,28 @@ def post_detail(request, year, month, day, post):
 		request,
 		'blog/post/detail.html',
 		{'post': post}
+	)
+
+
+def post_share(request, post_id):
+	form = EmailPostForm()
+	post = get_object_or_404(Post, id=post_id)
+	sent = False
+	data = {}
+	if request.method == 'POST':
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = "{} ({}) recommends you reading {}".format(data['name'], data['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+				post.title, post_url, data['name'], data['comments']
+			)
+			send_mail(subject, message, data['email'], [data['to']])
+			sent = True
+
+	return render(
+		request,
+		'blog/post/share.html',
+		{'post': post, 'form': form, 'sent': sent, 'data': data}
 	)
