d62fe5fe121392d21427cf980846093eefa39acc
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-10 00:12:34 +0300

    chore: add env sample

--- share/settings.py
+++ share/settings.py
@@ -18,12 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9hp*i(w+g7gw_6@w8c9!jweb6cl^3zhe_@zi)16$!m51x10^^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
